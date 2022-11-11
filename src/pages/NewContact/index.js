4ebdfcc0afcf117840df053c1a2f3150ff533015
RodrigoRedivo <rodrigoredivo@pop-os.localdomain>
2022-11-11 09:04:32 -0300

    feat: Creating the Button component

--- src/pages/NewContact/index.js
+++ src/pages/NewContact/index.js
@@ -2,6 +2,7 @@ import { PageHeader } from '../../components/PageHeader';
 
 import Input from '../../components/Input';
 import Select from '../../components/Select';
+import Button from '../../components/Button';
 
 export function NewContact() {
   return (
@@ -16,6 +17,13 @@ export function NewContact() {
         <option value="123">Instagram</option>
         <option value="123">Instagram</option>
       </Select>
+
+      <Button type="button">
+        Salvar Alterações
+      </Button>
+      <Button type="button" disabled>
+        Salvar Alterações
+      </Button>
     </>
   );
 }
