1d269275dcb78de723ad27e1731c5c441ced548b
RodrigoRedivo <rodrigoredivo@pop-os.localdomain>
2022-11-12 18:00:54 -0300

    feat: Creating the Loader component

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -7,13 +7,13 @@ import {
 import arrow from '../../assets/images/icons/arrow.svg';
 import edit from '../../assets/images/icons/edit.svg';
 import trash from '../../assets/images/icons/trash.svg';
-import { Modal } from '../../components/Modal';
+
+import { Loader } from '../../components/Loader';
 
 export function Home() {
   return (
     <Container>
-      <Modal />
-
+      <Loader />
       <InputSearchContainer>
         <input type="text" placeholder="Pesquise pelo nome..." />
       </InputSearchContainer>
