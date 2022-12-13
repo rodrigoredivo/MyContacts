a85e4ce5b7fb7e339f002c77a534c612e6ddbde4
RodrigoRedivo <rodrigoredivo@pop-os.localdomain>
2022-12-13 11:28:25 -0300

    feat: Componentizing the Spinner and making it reusable

--- src/components/FormGroup/index.js
+++ src/components/FormGroup/index.js
@@ -1,4 +1,5 @@
 import PropTypes from 'prop-types';
+import { Spinner } from '../Spinner';
 
 import { Container } from './styles';
 
@@ -7,7 +8,11 @@ export function FormGroup({ children, error, isLoading }) {
     <Container>
       <div className="form-item">
         {children}
-        {isLoading && <div className="loader" />}
+        {isLoading && (
+          <div className="loader">
+            <Spinner size={16} />
+          </div>
+        )}
       </div>
       {error && <small>{error}</small>}
     </Container>
