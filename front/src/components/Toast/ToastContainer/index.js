f32a7178e73638865c403a154d214b8a5e30dec7
RodrigoRedivo <rodrigoredivo@pop-os.localdomain>
2023-01-03 08:52:37 -0300

    feat: Context API? Not! Understanding the EventEmitter

--- front/src/components/Toast/ToastContainer/index.js
+++ front/src/components/Toast/ToastContainer/index.js
@@ -1,13 +1,23 @@
+import { useState } from 'react';
 import { ToastMessage } from '../ToastMessage';
 
 import { Container } from './styles';
 
 export function ToastContainer() {
+  const [messages] = useState([
+    { id: Math.random(), type: 'default', text: 'Default text' },
+    { id: Math.random(), type: 'success', text: 'Success text' },
+    { id: Math.random(), type: 'danger', text: 'Danger text' },
+  ]);
   return (
     <Container>
-      <ToastMessage text="Default toast" />
-      <ToastMessage text="Error toast" type="danger" />
-      <ToastMessage text="Success toast" type="success" />
+      {messages.map((message) => (
+        <ToastMessage
+          key={message.id}
+          type={message.type}
+          text={message.text}
+        />
+      ))}
     </Container>
   );
 }
